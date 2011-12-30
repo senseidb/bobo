d1ccb5f8fac8e0041656c3bab3b315b3325309a2
Geoff Cooney <cooney.geoff@gmail.com>
2011-12-30 11:35:01 -0500

    bobo-contrib:  Additional unit tests for GeoOnlyIndexer

--- bobo-contrib/test/com/browseengine/bobo/geosearch/solo/index/impl/GeoOnlyIndexerTest.java
+++ bobo-contrib/test/com/browseengine/bobo/geosearch/solo/index/impl/GeoOnlyIndexerTest.java
@@ -9,10 +9,14 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.Lock;
+import org.apache.lucene.store.LockFactory;
+import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.store.RAMDirectory;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.annotation.IfProfileValue;
@@ -46,6 +50,8 @@ public class GeoOnlyIndexerTest {
     private GeoSegmentWriter<IDGeoRecord> mockGeoSegmentWriter;
     private IGeoConverter geoConverter;
     
+    private Lock mockLock;
+    
     Set<IDGeoRecord> lastDataFlushed = null;
     
     @SuppressWarnings("unchecked")
@@ -54,10 +60,23 @@ public class GeoOnlyIndexerTest {
         indexName = UUID.randomUUID().toString();
         config = new GeoSearchConfig();
         directory = new RAMDirectory();
+        directory.setLockFactory(new LockFactory() {
+            @Override
+            public void clearLock(String arg0) throws IOException {
+                //do nothing
+            }
+
+            @Override
+            public Lock makeLock(String arg0) {
+                return mockLock;
+            }
+        });
         
         mockGeoSegmentReader = context.mock(GeoSegmentReader.class);
         mockGeoSegmentWriter = context.mock(GeoSegmentWriter.class);
-        
+        mockLock = context.mock(Lock.class);
+
+        expectLockObtain();
         geoConverter = new GeoConverter();
         
         indexer = new GeoOnlyIndexer(config, directory, indexName) {
@@ -72,6 +91,23 @@ public class GeoOnlyIndexerTest {
                 return mockGeoSegmentReader;
             }
         };
+        
+        context.assertIsSatisfied();
+    }
+    
+    @After
+    public void tearDown() {
+        context.assertIsSatisfied();
+    }
+                            
+    
+    private void expectLockObtain() throws IOException {
+        context.checking(new Expectations() {
+            {
+                one(mockLock).obtain();
+                will(returnValue(true));
+            }
+        });
     }
     
     @Test
@@ -119,6 +155,8 @@ public class GeoOnlyIndexerTest {
                 one(mockGeoSegmentReader).close();
                 
                 one(mockGeoSegmentWriter).close();
+                
+                one(mockLock).release();
             }
         });
         
@@ -127,6 +165,44 @@ public class GeoOnlyIndexerTest {
         context.assertIsSatisfied();
         
         assertEquals(countEntries + existingData.size(), lastDataFlushed.size());
+    }
+    
+    @Test(expected = IOException.class)
+    public void index_and_flush_error_on_read() throws IOException {
+        int idByteLength = GeoOnlyIndexer.IDByteCount;
+        String fieldName = "field1";
+        
+        for (int i = 0; i < 10; i++) {
+            byte[] uuid = generateRandomUUIDAsBytes(idByteLength);
+            GeoCoordinate geoCoordinate = new GeoCoordinate(randomLattitude(), randomLongitude());
+            GeoCoordinateField field = new GeoCoordinateField(fieldName, geoCoordinate);
+            
+            indexer.index(uuid, field);
+        }
+
+        context.checking(new Expectations() {
+            { 
+                one(mockGeoSegmentReader).getIterator(with(IDGeoRecord.MIN_VALID_GEORECORD), with(IDGeoRecord.MAX_VALID_GEORECORD));
+                will(throwException(new IOException()));
+                
+                one(mockGeoSegmentReader).close();
+                one(mockLock).release();
+            }
+        });
+        
+        indexer.flush();
+    }
+    
+    @Test(expected = LockObtainFailedException.class)
+    public void index_and_flush_lockObtainFailed() throws IOException {
+        context.checking(new Expectations() {
+            {
+                one(mockLock).obtain();
+                will(returnValue(false));
+            }
+        });
+        
+        GeoOnlyIndexer indexer = new GeoOnlyIndexer(config, directory, indexName);
     }
     
     private double randomLongitude() {
