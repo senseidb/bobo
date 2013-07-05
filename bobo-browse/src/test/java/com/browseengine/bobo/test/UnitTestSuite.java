cc412aef3810d34a29bbf7d4aec552c2d4cee671
Yonghui Zhao <zhaoyonghui@gmail.com>
2013-07-05 11:42:24 +0800

    Remove analysisi section

--- bobo-browse/src/test/java/com/browseengine/bobo/test/UnitTestSuite.java
+++ bobo-browse/src/test/java/com/browseengine/bobo/test/UnitTestSuite.java
@@ -1,7 +1,7 @@
 /**
- * Bobo Browse Engine - High performance faceted/parametric search implementation 
+ * Bobo Browse Engine - High performance faceted/parametric search implementation
  * that handles various types of semi-structured data.  Written in Java.
- * 
+ *
  * Copyright (C) 2005-2006  John Wang
  *
  * This library is free software; you can redistribute it and/or
@@ -17,9 +17,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- * To contact the project administrators for the bobo-browse project, 
- * please go to https://sourceforge.net/projects/bobo-browse/, or 
+ *
+ * To contact the project administrators for the bobo-browse project,
+ * please go to https://sourceforge.net/projects/bobo-browse/, or
  * send mail to owner@browseengine.com.
  */
 
@@ -29,26 +29,22 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import junit.textui.TestRunner;
 
-import com.browseengine.bobo.test.section.TestSectionSearch;
-
-
 public class UnitTestSuite {
 
-	  public static Test suite(){
-          TestSuite suite=new TestSuite();
-          suite.addTestSuite(BoboTestCase.class);
-          suite.addTestSuite(FacetHandlerTest.class);
-          suite.addTestSuite(TestSectionSearch.class);
-          suite.addTestSuite(BoboFacetIteratorTest.class);
-          suite.addTestSuite(FacetNotValuesTest.class);
-          suite.addTestSuite(FacetNameTest.class);
-          return suite;
+  public static Test suite() {
+    TestSuite suite = new TestSuite();
+    suite.addTestSuite(BoboTestCase.class);
+    suite.addTestSuite(FacetHandlerTest.class);
+    suite.addTestSuite(BoboFacetIteratorTest.class);
+    suite.addTestSuite(FacetNotValuesTest.class);
+    suite.addTestSuite(FacetNameTest.class);
+    return suite;
   }
-  
+
   /**
    * @param args
    */
   public static void main(String[] args) {
-          TestRunner.run(suite());
+    TestRunner.run(suite());
   }
 }
