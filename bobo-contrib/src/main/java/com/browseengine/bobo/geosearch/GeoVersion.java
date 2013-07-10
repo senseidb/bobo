87dade14c00cb613d061542884e4ae302cb3dc09
Yonghui Zhao <zhaoyonghui@gmail.com>
2013-07-10 20:14:29 +0800

    Remove bobo-contrib

--- bobo-contrib/src/main/java/com/browseengine/bobo/geosearch/GeoVersion.java
+++
@@ -1,9 +0,0 @@
-package com.browseengine.bobo.geosearch;
-
-public class GeoVersion {
-    public static final int CURRENT_VERSION = 1;
-    public static final int CURRENT_GEOONLY_VERSION = 1;
-    
-    public static final int VERSION_0 = 0;
-    public static final int VERSION_1 = 1;
-}
